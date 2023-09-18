6312ba2060718cd1d1e5b0b97c341d8761292cf4
Svetlana-Gubina <gubinas83@gmail.com>
2023-09-18 11:07:10 +0300

    fix tests

--- 02-components/03-MeetupCover/components/MeetupCover.js
+++ 02-components/03-MeetupCover/components/MeetupCover.js
@@ -6,30 +6,15 @@ export default defineComponent({
   props: {
     title: {
       type: String,
-<<<<<<< HEAD
     },
 
     image: {
       type: String,
-=======
-      default: '',
-    },
-    image: {
-      type: String,
-      default: '',
->>>>>>> 81d0cc8 (Fix MeetupDescription, implement MeetupCover)
     },
   },
 
   template: `
-<<<<<<< HEAD
     <div class="meetup-cover" :style="image && { '--bg-url': \`url('\${image}')\` }">
       <h1 class="meetup-cover__title">{{ title }}</h1>
-=======
-    <div class="meetup-cover" :style="\`--bg-url: url(\${image})\`">
-        <!-- Ссылка выше - пример верстки при наличии изображения, а не изображение по умолчанию. -->
-        <!-- Изображение по умолчанию уже есть в стилях. Для его вывода достаточно не добавлять никаких стилей. -->
-        <h1 v-if="title" class="meetup-cover__title">{{ title }}</h1>
->>>>>>> 81d0cc8 (Fix MeetupDescription, implement MeetupCover)
     </div>`,
 });
