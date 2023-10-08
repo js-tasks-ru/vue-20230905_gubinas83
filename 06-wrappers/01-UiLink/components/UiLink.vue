939b71ebe8cff0bb31248b9e3736cd648d1eeb25
Svetlana-Gubina <gubinas83@gmail.com>
2023-10-08 14:56:23 +0300

    Implement UiButton

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,33 +1,17 @@
 <template>
-<<<<<<< HEAD
   <component :is="tag" class="link"><slot /></component>
-=======
-  <component :is="tag" class="link" v-bind="$attrs">
-  <slot />
-  </component>
->>>>>>> a908b59 (Implement UiLink)
 </template>
 
 <script>
 export default {
   name: 'UiLink',
-<<<<<<< HEAD
-=======
-  inheritAttrs: false,
->>>>>>> a908b59 (Implement UiLink)
 
   props: {
     tag: {
       type: [String, Object, Function],
-<<<<<<< HEAD
       default: 'router-link',
     },
   },
-=======
-      default: 'router-link'
-    }
-  }
->>>>>>> a908b59 (Implement UiLink)
 };
 </script>
 
